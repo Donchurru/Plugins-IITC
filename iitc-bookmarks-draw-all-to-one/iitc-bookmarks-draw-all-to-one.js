7ca921760840742d01107aa816a306f4c43d67e6
Ruben Romero <rubenrmatias@gmail.com>
2019-02-27 18:36:47 +0100

    Cambio referencias cabecera a master

--- iitc-bookmarks-draw-all-to-one/iitc-bookmarks-draw-all-to-one.js
+++ iitc-bookmarks-draw-all-to-one/iitc-bookmarks-draw-all-to-one.js
@@ -3,9 +3,9 @@
 // @name IITC Plugin: Draw All To One.
 // @category Controls
 // @version 0.0.1
-// @namespace https://github.com/Donchurru/Plugins-IITC/tree/develop/iitc-bookmarks-draw-all-to-one
-// @updateURL      https://github.com/Donchurru/Plugins-IITC/blob/develop/iitc-bookmarks-draw-all-to-one/iitc-bookmarks-draw-all-to-one.js
-// @downloadURL    https://raw.githubusercontent.com/Donchurru/Plugins-IITC/develop/iitc-bookmarks-draw-all-to-one/iitc-bookmarks-draw-all-to-one.js
+// @namespace https://github.com/Donchurru/Plugins-IITC/tree/master/iitc-bookmarks-draw-all-to-one
+// @updateURL      https://github.com/Donchurru/Plugins-IITC/blob/master/iitc-bookmarks-draw-all-to-one/iitc-bookmarks-draw-all-to-one.js
+// @downloadURL    https://raw.githubusercontent.com/Donchurru/Plugins-IITC/master/iitc-bookmarks-draw-all-to-one/iitc-bookmarks-draw-all-to-one.js
 // @author Donchurru
 // @description Draw one portal to others portal in the same folder
 // @include        https://*.ingress.com/intel*
